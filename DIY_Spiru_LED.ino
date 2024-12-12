6c68109c6c68f34d16362e419941c3ef177a4c67
Hybridoa <Vlorenzolana@users.noreply.github.com>
2024-12-12 13:19:48 +0100

    Update DIY_Spiru_LED.ino
    UPDATE WITH LED UV LIGHT

--- DIY_Spiru_LED.ino
+++ DIY_Spiru_LED.ino
@@ -1,21 +1,27 @@
-int PUMPin = 4;    //This is the output PUMPin on the Arduino we are using
-int LED = 9;
+int PUMPin = 4; //This is the output PUMPin on the Arduino we are using
+int LED = 9;    //LED WHITE
+int LED2 = 3;    //LED UV
+
 
 void setup() {
-  Serial.begin(19200);
-  // put your setup code here, to run once:
-  pinMode(PUMPin, OUTPUT);           // Sets the PUMPin as an output
-  pinMode(LED, OUTPUT);         //LED Sets the PUMPin as an output
+  Serial.begin(19200);        // your setup code here, to run once:
+  pinMode(PUMPin, OUTPUT);   // Sets the PUMPin as an output
+  pinMode(LED, OUTPUT);     //Sets the LED as an output
+  pinMode(LED2, OUTPUT);    //Sets the LED2 as an output
 }
  
 void loop() {
   // put your main code here, to run repeatedly:  
   digitalWrite(PUMPin, HIGH);  //Switch  ON
-  delay(10000);                      //Wait 1 Second
+  delay(90000);                      //Wait 90 Second
   digitalWrite(PUMPin, LOW);   //Switch  OFF
   delay(1000);                      // Wait 1 second
   digitalWrite(LED, HIGH);          // LED ON
-  delay(20000);                     // Wait 20 Second
+  delay(10000);                     // Wait 10 Second
   digitalWrite(LED, LOW);           // LED OFF
-  delay(900000);                     // Wait 15min 900second
+  delay(1000);                     // Wait LED 1second
+  digitalWrite(LED2, HIGH);          // LED UV  ON
+  delay(90000);                     // Wait 90 Second
+  digitalWrite(LED2, LOW);           // LED UV OFF
+  delay(1000);                     // Wait 1 second
 }
